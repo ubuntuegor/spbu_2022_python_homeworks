abaf5e1e0560901547f8359c710f78249273ea88
Egor Kukoverov <me@bnt.to>
2022-03-07 20:15:03 +0300

    remove unchanged args from closure

--- src/hw2/currying.py
+++ src/hw2/currying.py
@@ -7,21 +7,11 @@ def curry_explicit(f: Callable, n: int) -> Callable:
     if n < 0:
         raise ValueError("Function cannot have a negative number of arguments")
 
-    def closure(f: Callable, n: int, args: Tuple) -> Callable:
+    def closure(n: int, args: Tuple) -> Callable:
         if n == 0:
             return lambda: f(*args)
         if n == 1:
             return lambda x: f(*args, x)
-        return lambda x: closure(f, n - 1, args + (x,))
+        return lambda x: closure(n - 1, args + (x,))
 
-    return closure(f, n, ())
-
-
-f2 = curry_explicit((lambda x, y, z: f"<{x},{y},{z}>"), 3)
-f1 = curry_explicit((lambda: "Hello!"), 0)
-ff = f2(123)(456)
-fd = f2(123)(987)
-print(ff(4444))
-print(fd(7686))
-print(f1())
-curry_explicit(print, 2)(1)(2)
+    return closure(n, ())
