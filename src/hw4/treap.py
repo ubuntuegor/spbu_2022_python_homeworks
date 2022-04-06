f9373f009b96f8c34adefc07ec514b2071492a58
Egor Kukoverov <me@bnt.to>
2022-04-07 00:08:40 +0300

    less complexity

--- src/hw4/treap.py
+++ src/hw4/treap.py
@@ -1,8 +1,9 @@
+import sys
 from abc import ABCMeta, abstractmethod
+from itertools import zip_longest
 from operator import attrgetter
 from random import randint
-import sys
-from typing import Generic, Optional, Tuple, TypeVar
+from typing import Any, Generic, Optional, TypeVar, cast
 
 
 class Comparable(metaclass=ABCMeta):
@@ -43,7 +44,8 @@ class _TreapNode(Generic[K]):
                 return None
             return self.left.find_child(key)
 
-    def split(self, key: K) -> Tuple["Optional[_TreapNode[K]]", "Optional[_TreapNode[K]]", "Optional[_TreapNode[K]]"]:
+    def split(self, key: K) -> tuple["Optional[_TreapNode[K]]", "Optional[_TreapNode[K]]", "Optional[_TreapNode[K]]"]:
+        """Splits the tree and returns 3 subtrees: less than `key`, greater than `key`, equal to `key`"""
         if key > self.key:
             parts = self.right.split(key) if self.right is not None else (None, None)
             self.right = parts[0]
@@ -56,6 +58,7 @@ class _TreapNode(Generic[K]):
             return (self.left, self.right, self)
 
     def merge_with(self, other: "Optional[_TreapNode[K]]") -> "_TreapNode[K]":
+        """Makes a new tree out of `self` and `other`. Each key in `self` must be smaller than any key in `other`"""
         if other is None:
             return self
 
@@ -65,51 +68,6 @@ class _TreapNode(Generic[K]):
         else:
             other.left = self.merge_with(other.left)
             return other
-
-
-class _TreapTree(Generic[K]):
-    def __init__(self, *nodes: _TreapNode[K]):
-        queue: list[_TreapNode[K] | None] = sorted(nodes, key=attrgetter("key"))
-
-        while len(queue) > 1:
-            new_queue: list[_TreapNode[K] | None] = []
-            if len(queue) % 2 != 0:
-                queue.append(None)
-            for left, right in zip(queue[::2], queue[1::2]):
-                if left is not None:
-                    new_queue.append(left.merge_with(right))
-            queue = new_queue
-
-        self._root: Optional[_TreapNode[K]] = queue[0] if len(queue) > 0 else None
-
-    def __iter__(self):
-        if self._root is None:
-            return iter(())
-        return iter(self._root)
-
-    def clear(self):
-        self._root = None
-
-    def find_node(self, key: K) -> Optional[_TreapNode[K]]:
-        if self._root is None:
-            return None
-        return self._root.find_child(key)
-
-    def insert(self, node: _TreapNode[K]):
-        if self._root is not None:
-            left, right, _ = self._root.split(node.key)
-            if left is not None:
-                self._root = left.merge_with(node).merge_with(right)
-            else:
-                self._root = node.merge_with(right)
-        else:
-            self._root = node
-
-    def remove(self, key: K):
-        if self._root is None:
-            return
-        left, right, _ = self._root.split(key)
-        self._root = left.merge_with(right) if left is not None else right
 
 
 def _random_int():
@@ -124,27 +82,40 @@ class Treap(Generic[K]):
     to use integer values as they are the fastest to compare.
     """
 
-    def __init__(self, from_dict: dict | None = None, /):
+    def __init__(self, from_dict: Optional[dict[K, Any]] = None, /):
         """Create a treap and fill it with values from the optional `dict` dictionary."""
         self._size: int = 0
-        if from_dict is not None:
+        self._root: Optional[_TreapNode] = None
+
+        if from_dict is not None and len(from_dict) > 0:
             self._size = len(from_dict)
-            nodes = (_TreapNode(key, value, _random_int()) for key, value in from_dict.items())
-            self._tree: _TreapTree[K] = _TreapTree(*nodes)
-        else:
-            self._tree = _TreapTree()
+            nodes = [_TreapNode(key, value, _random_int()) for key, value in from_dict.items()]
+            queue: list[_TreapNode[K]] = sorted(nodes, key=attrgetter("key"))
+
+            while len(queue) > 1:
+                new_queue: list[_TreapNode[K]] = []
+                for left, right in zip_longest(queue[::2], queue[1::2]):
+                    if left is not None:
+                        new_queue.append(left.merge_with(right))
+                queue = new_queue
+
+            self._root = queue[0]
+
+    def _find_node(self, key: K) -> Optional[_TreapNode]:
+        return self._root and self._root.find_child(key)
 
     def clear(self):
         self._size = 0
-        self._tree.clear()
+        self._root = None
 
     def get(self, key: K):
-        node = self._tree.find_node(key)
+        node = self._find_node(key)
         return node.value if (node is not None) else None
 
     def __iter__(self):
-        for node in self._tree:
-            yield node.key
+        if self._root is not None:
+            for node in self._root:
+                yield node.key
 
     def __repr__(self):
         pairs = []
@@ -156,27 +127,36 @@ class Treap(Generic[K]):
         return self._size
 
     def __contains__(self, key: K):
-        node = self._tree.find_node(key)
+        node = self._find_node(key)
         return node is not None
 
     def __getitem__(self, key: K):
-        node = self._tree.find_node(key)
+        node = self._find_node(key)
         if node is None:
             raise KeyError(repr(key))
         return node.value
 
     def __setitem__(self, key: K, value):
-        node = self._tree.find_node(key)
+        node = self._find_node(key)
         if node is not None:
             node.value = value
         else:
             self._size += 1
-            self._tree.insert(_TreapNode(key, value, _random_int()))
+            node = _TreapNode(key, value, _random_int())
+            if self._root is not None:
+                left, right, _ = self._root.split(node.key)
+                if left is not None:
+                    self._root = left.merge_with(node).merge_with(right)
+                else:
+                    self._root = node.merge_with(right)
+            else:
+                self._root = node
 
     def __delitem__(self, key: K):
-        node = self._tree.find_node(key)
+        node = self._find_node(key)
         if node is not None:
             self._size -= 1
-            self._tree.remove(key)
+            left, right, _ = cast(_TreapNode[K], self._root).split(key)
+            self._root = left.merge_with(right) if left is not None else right
         else:
             raise KeyError(repr(key))
