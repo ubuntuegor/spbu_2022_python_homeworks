b938da27427a5ad10d413e70967613a6aaac3aeb
Egor Kukoverov <me@bnt.to>
2022-04-29 15:17:56 +0300

    change to array and add test

--- src/test2/quicksort.py
+++ src/test2/quicksort.py
@@ -1,13 +1,14 @@
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Array
+from array import array
 
 
-def _swap_el(arr: list, i: int, j: int):
+def _swap_el(arr: Array, i: int, j: int):
     tmp = arr[i]
     arr[i] = arr[j]
     arr[j] = tmp
 
 
-def _partition(arr: list, start: int, end: int) -> int:
+def _partition(arr: Array, start: int, end: int) -> int:
     pivot = arr[end - 1]
     j = start - 1
 
@@ -21,13 +22,13 @@ def _partition(arr: list, start: int, end: int) -> int:
     return j
 
 
-def _quick_sort_rec(arr: list, start: int, end: int, depth: int):
+def _quick_sort_rec(arr: Array, start: int, end: int, depth: int):
     if end - start <= 1:
         return
 
     pivot_i = _partition(arr, start, end)
 
-    if depth < 2:
+    if depth < 3:
         p = Process(target=_quick_sort_rec, args=(arr, start, pivot_i, depth + 1))
         p.start()
         _quick_sort_rec(arr, pivot_i + 1, end, depth + 1)
@@ -37,8 +38,7 @@ def _quick_sort_rec(arr: list, start: int, end: int, depth: int):
         _quick_sort_rec(arr, pivot_i + 1, end, depth + 1)
 
 
-def quick_sort(arr: list) -> list:
-    with Manager() as manager:
-        shared_list = manager.list(arr)
-        _quick_sort_rec(shared_list, 0, len(arr), 0)
-        return list(shared_list)
+def quick_sort(arr: array) -> array:
+    shared_arr = Array(arr.typecode, arr, lock=False)
+    _quick_sort_rec(shared_arr, 0, len(arr), 0)
+    return array(arr.typecode, shared_arr)
